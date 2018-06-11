4647670c13813424681b856081e1dd64899be100
Jesse <gallardotjesse@gmail.com>
2018-06-11 16:25:24 -0400

    1)Added functionality to create a desktop folder and check if said folder exists. This folder will store all files created by the user for the use of this program.
    2)Added functionality to create new files that the user will have to
    load in order to save their data

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include <QtWidgets>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
 #include <stdio.h>
+#include <dirent.h>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "BudgetSheet.h"
@@ -11,6 +15,8 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    checkDirectory();
     setMessages();
 }
 
@@ -21,4 +27,46 @@ MainWindow::~MainWindow()
 
 void MainWindow::setMessages(){
     ui->statusBar->showMessage(tr("Welcome, please click the help button to know how to use this app!"));
+}
+
+void MainWindow::checkDirectory(){
+    DIR *check;
+    string path(getenv("HOME"));
+    path += "/Desktop/BudgetSheet";
+    const char *tpath = path.c_str();
+    check = opendir(tpath);
+    if(!check){
+        string command = "mkdir -p " + path;
+        const char *tcommand = command.c_str();
+        const int test = system(tcommand);
+        if(test == -1){
+            cout << "fuck" << endl;
+        }
+    }
+    else{
+        cout << "cool" << endl;
+    }
+}
+
+void MainWindow::on_CreateButton_clicked()
+{
+    //This works for Mac and possibly Linux, unsure about Windows
+    string path(getenv("HOME"));
+    path += "/Desktop/BudgetSheet/";
+
+    QString qmonth = ui->CreateMonth->currentText();
+    QString qyear = ui->CreateYear->text();
+
+    if(qyear == ""){
+        return;
+    }
+
+    string month = qmonth.toStdString();
+    string year = qyear.toStdString();
+
+    string fname = month + "-" + year + ".txt";
+
+    path += fname;
+
+    ofstream newf(path);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,9 +15,13 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void setMessages();
+    void checkDirectory();
+    //void createFile();
 
 private slots:
 
+
+    void on_CreateButton_clicked();
 
 private:
     Ui::MainWindow *ui;
